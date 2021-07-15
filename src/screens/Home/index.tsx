82e3c360de3128333214158a25267d224e0e3055
gamonpo <gamonpo@outlook.com>
2021-07-15 11:36:57 -0300

    Create Header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container, Title } from "./styles";
+import Logo from "../../assets/logo.svg";
+
+import { Container, Header, TotalCars, HeaderContent } from "./styles";
 
 const Home: React.FC = () => (
   <Container>
-    <Title>Home</Title>
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+    <Header>
+      <HeaderContent>
+        <Logo width={RFValue(108)} height={RFValue(12)} />
+
+        <TotalCars>Total de 12 carros</TotalCars>
+      </HeaderContent>
+    </Header>
   </Container>
 );
 
