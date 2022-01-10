0db98e20d2f4f986ac4dcc3b8345b18e0b70f832
gamonpo <gamonpo@outlook.com>
2022-01-10 11:23:57 -0300

    Create custom button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { useTheme } from 'styled-components';
 
 import { Container, Title } from './styles';
@@ -7,14 +8,31 @@ interface Props {
   title: string;
   color?: string;
   onPress: () => void;
+  enabled?: boolean;
+  loading?: boolean;
 }
 
-export function Button({ title, color, onPress }: Props) {
+export function Button({
+  title,
+  color,
+  onPress,
+  enabled = true,
+  loading = false,
+}: Props) {
   const theme = useTheme();
 
   return (
-    <Container color={color || theme.colors.main} onPress={onPress}>
-      <Title>{title}</Title>
+    <Container
+      color={color || theme.colors.main}
+      onPress={onPress}
+      enabled={enabled}
+      style={{ opacity: enabled === false || loading === true ? 0.5 : 1 }}
+    >
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape} />
+      ) : (
+        <Title>{title}</Title>
+      )}
     </Container>
   );
 }
