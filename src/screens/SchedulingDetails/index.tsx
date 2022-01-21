9ad67ada31c7de04118c1abb8936de141fbc3742
gamonpo <gamonpo@outlook.com>
2022-01-21 13:17:50 -0300

    ref(): Change target navigation screen

--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -90,7 +90,13 @@ export function SchedulingDetails() {
         id: car.id,
         unavailable_dates,
       })
-      .then(() => navigation.navigate('SchedulingComplete'))
+      .then(() =>
+        navigation.navigate('Confirmation', {
+          title: 'Carro alugado!',
+          message: `Agora você só precisa ir\n até a concessionária da RENTX`,
+          nextScreenRoute: 'Home',
+        }),
+      )
       .catch(() => {
         setLoading(false);
         Alert.alert('Não foi possível confirmar o agendamento.');
