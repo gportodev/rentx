5bf901d1fd4f4f469a0f585af5313a79b216b9f2
gamonpo <gamonpo@outlook.com>
2022-01-21 13:19:03 -0300

    feat(): Create SignUpSecondStep screen

---
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -0,0 +1,102 @@
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { Alert, Keyboard, KeyboardAvoidingView } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../../components/BackButton';
+import { Bullet } from '../../../components/Bullet';
+import Button from '../../../components/Button';
+import { PasswordInput } from '../../../components/PasswordInput';
+
+import {
+  Container,
+  Header,
+  Steps,
+  Title,
+  SubTitle,
+  Form,
+  FormTitle,
+} from './styles';
+
+interface Params {
+  user: {
+    name: string;
+    email: string;
+    driverLicense: string;
+  };
+}
+
+export function SignUpSecondStep() {
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const navigation = useNavigation();
+  const route = useRoute();
+  const theme = useTheme();
+
+  const { user } = route.params as Params;
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  function handleRegister() {
+    if (!password || !passwordConfirm) {
+      return Alert.alert('Preencha ambos os campos');
+    }
+
+    if (password !== passwordConfirm) {
+      return Alert.alert('Senhas não conferem');
+    }
+
+    navigation.navigate('Confirmation', {
+      title: 'Conta Criada!',
+      message: `Agora é só fazer login\n e aproveitar`,
+      nextScreenRoute: 'SignIn',
+    });
+  }
+
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
+
+          <Title>Crie sua {'\n'}conta</Title>
+          <SubTitle>
+            Faça seu cadastro de {'\n'}
+            forma rápida e fácil
+          </SubTitle>
+
+          <Form>
+            <FormTitle>2. Senha</FormTitle>
+
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              onChangeText={setPassword}
+              value={password}
+            />
+            <PasswordInput
+              iconName="lock"
+              placeholder="Repetir Senha"
+              onChangeText={setPasswordConfirm}
+              value={passwordConfirm}
+            />
+          </Form>
+
+          <Button
+            color={theme.colors.success}
+            title="Cadastrar"
+            onPress={handleRegister}
+          />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
